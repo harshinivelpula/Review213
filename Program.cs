bdc8581838c9d659c0fa7837d8be5f9c1607701a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-15 12:10:40 +0530

    checkleapyear

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+namespace leapyearday
+{
+    public static class program
+    {
+        public static void Main()
+        {
+            int checkyear;
+
+            Console.Write("Check whether a given year is leap year or not");
+
+
+            Console.Write("Input an year : ");
+            checkyear = Convert.ToInt32(Console.ReadLine());
+
+            if ((checkyear % 400) == 0)
+                Console.WriteLine("{0} is a leap year", checkyear);
+            else if ((checkyear % 100) == 0)
+                Console.WriteLine("{0} is not a leap year", checkyear);
+            else if ((checkyear % 4) == 0)
+                Console.WriteLine("{0} is a leap year", checkyear);
+            else
+                Console.WriteLine("{0} is not a leap year", checkyear);
+        }
+    }
+}
+
